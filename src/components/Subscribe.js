f2b3427de4416f670d14f4ad4e29908909107977
Chibuike Ewenike <sopewenike@yahoo.com>
2023-03-01 15:39:24 +0100

    subscribe finished

--- src/components/Subscribe.js
+++ src/components/Subscribe.js
@@ -2,42 +2,49 @@ import React from "react";
 
 function Subscribe() {
   return (
-    <div className="bg-[#037fb0] pt-[32px] px-[8px] pb-[40px]">
+    <div className="bg-[#037fb0] text-[#fff] pt-[32px] px-[8px] pb-[40px]">
       <div>
-        <h1 className="text-center font-medium tracking-wide text-[1.75rem] pb-4">
+        <h1 className="text-center font-semibold tracking-wide text-[1.5rem] break-words">
           Stay informed with email updates
         </h1>
 
-        <p className="pt-4 text-[#95a0ab] text-sm mb-4 text-center">
+        <p className="mt-3 text-base mb-4 text-center">
           Take your financial knowledge further with PenFed Credit Union.
         </p>
       </div>
-      <div className="md:flex gap-5">
-        <input
-          type="text"
-          placeholder="First Name"
-          className="basis-[30%]"
-        ></input>
-        <input
-          type="text"
-          placeholder="Last Name"
-          className="basis-[30%]"
-        ></input>
-        <input
-          type="text"
-          placeholder="Email Address"
-          className="basis-[30%]"
-        ></input>
+      <div className=" md:flex flex-wrap gap-5 mt-8 px-2 w-full xs:max-w-[544px] md:max-w-[720px] penfedL:max-w-[960px] lg:max-w-[1200px] mx-auto">
+        <div className="basis-[30%] pb-8">
+          <input
+            type="text"
+            className=" px-2 php py-2 rounded w-full"
+            placeholder="First Name"
+          ></input>
+        </div>
+        <div className="basis-[30%] pb-8">
+          <input
+            type="text"
+            className=" px-2 php py-2 rounded w-full"
+            placeholder="Last Name"
+          ></input>
+        </div>
+        <div className="basis-[30%] pb-8">
+          <input
+            type="text"
+            className=" px-2 php py-2 rounded w-full"
+            placeholder="Email Address"
+          ></input>
+        </div>
+        <button className="bg-[#047dba] flex justify-center  mx-auto cursor-pointer py-[0.375rem] px-[1.375rem] text-base font-[700] border rounded-[6px] border-solid border-[#ffffff]">
+          Sign Up
+        </button>
       </div>
-      <div>
-        <p>
+
+      <div className="mt-4  px-2 w-full xs:max-w-[544px] md:max-w-[720px] penfedL:max-w-[960px] lg:max-w-[1200px] mx-auto">
+        <p className="text-center">
           Your information is safe with us. We will only use it to deliver
           content and promotions. You can always unsubscribe at any time.
         </p>
       </div>
-      <button className="bg-[#047dba] cursor-pointer py-[0.375rem] px-[1.375rem] text-base font-[500] border rounded-[6px] border-solid border-[#398007]">
-        Locate ATM
-      </button>
     </div>
   );
 }
