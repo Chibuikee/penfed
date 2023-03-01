35703d1c383e7ebdde36db37fdff9091f515ce10
Chibuike Ewenike <sopewenike@yahoo.com>
2023-03-01 22:17:01 +0100

    top nav completed

--- src/components/heroSection/Hero.js
+++ src/components/heroSection/Hero.js
@@ -9,21 +9,28 @@ function Hero() {
           backgroundImage:
             "linear-gradient(rgba(0,0,0,0.827),rgba(0,0,0,0.3)),url('https://www.penfed.org/content/dam/penfed/general/img/2023/homepage/auto-promo-r1-billboard-1660x400.jpg')",
         }}
-        className="text-[white] text-[1.5rem] bg-right bg-cover w-[100%] h-[420px] relative"
+        className="text-[#fff] text-[1.5rem] bg-right bg-cover w-[100%] h-[420px] relative"
       >
-        <div className="h-full w-full absolute top-0 left-0 text-[#ffffff] text-[20px]">
-          <h2>AUTOS PROMOTION</h2>
-          <div className="w-[100px] h-[10px] bg-[red]"></div>
-          <h1 className="text-[32px]"> $350 Bonus with Vehicle Purchase</h1>
+        <div className="h-full mx-auto xs:max-w-[544px] md:max-w-[720px] penfedL:max-w-[960px] lg:max-w-[1200px] absolute top-0 left-0 right-0 text-[#ffffff] text-[20px] px-[36px]">
+          <h2 className="mt-[8px] text-base font-[700] tracking-[3px] pt-[24px]">
+            AUTOS PROMOTION
+          </h2>
+          <div className="w-[200px] h-[5px] bg-[#047dba] "></div>
+          <h1 className="text-[32px] font-[700] pt-[24px]">
+            {" "}
+            $350 Bonus with Vehicle Purchase
+          </h1>
+          <ul className="list-disc ml-[24px] mt-2">
+            <li className="text-base  ">
+              When you use the PenFed Car Buying Service and a PenFed auto loan
+            </li>
+            <li className="text-base mt-2">Rates as low as 4.89% APR*</li>
+          </ul>
 
-          <h3>
-            When you use the PenFed Car Buying Service and a PenFed auto loan
-          </h3>
-          <h3>Rates as low as 4.89% APR*</h3>
-          <button className="bg-[#047dba] cursor-pointer py-[0.375rem] px-[1.375rem] text-base font-[500] border rounded-[6px] border-solid border-[#398007]">
+          <button className="bg-[#047dba] mt-5 cursor-pointer py-[0.375rem] px-[1.375rem] text-base font-[600] border rounded-[6px] border-solid border-[#398007]">
             Find a Car
           </button>
-          <h3>Offer ends April 30, 2023</h3>
+          <h3 className="text-xs font-[600] mt-5">Offer ends April 30, 2023</h3>
         </div>
       </div>
       <img src="" alt="company car" />
--- src/components/navBar/NavBar.js
+++ src/components/navBar/NavBar.js
@@ -1,8 +1,69 @@
 import React from "react";
+import { FaBars } from "react-icons/fa";
+import { RiCloseFill } from "react-icons/ri";
+import { useState } from "react";
+
 function NavBar() {
+  const [navBarToggle, setNavBarToggle] = useState(false);
+
+  const data = [
+    { name: "HOME" },
+    { name: "SERVICES" },
+    { name: "PRODUCTS" },
+    { name: "CONTACT" },
+    { name: "ABOUT" },
+  ];
+  const ADdata = [
+    {
+      name: "ROUTING # 256078446",
+      url: "#",
+    },
+    {
+      name: "PARTNERS",
+      url: "#",
+    },
+    {
+      name: "MEMBER DISCOUNTS",
+      url: "#",
+    },
+    {
+      name: "WEALTH",
+      url: "#",
+    },
+    {
+      name: "FOUNDATION",
+      url: "#",
+    },
+    {
+      name: "BRANCHES & ATMS",
+      url: "#",
+    },
+    {
+      name: "ABOUT",
+      url: "#",
+    },
+    {
+      name: "SEARCH",
+      url: "#",
+    },
+  ];
+
   return (
     <nav className="">
-      <div className="p-5 flex justify-between">
+      <div className="p-[6px] bg-[#f4f4f4] hidden lg:block">
+        <ul className="flex justify-between ml-[150px] ">
+          {ADdata.map((item) => (
+            <li
+              className={`${
+                item.name == "SEARCH" ? "text-[red]" : "text-[#282828]"
+              }] text-[.75rem]`}
+            >
+              {item.name}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="p-[27px] flex justify-between mr-7">
         <img
           src={process.env.PUBLIC_URL + "/logo.png"}
           className="w-[140px] h-[35.5px]"
@@ -10,6 +71,28 @@ function NavBar() {
         <button className="bg-[#047dba] cursor-pointer py-[0.375rem] px-[1.375rem] text-base font-[500] border rounded-[6px] border-solid border-[#398007]">
           Login
         </button>
+      </div>
+      <div className=" absolute s:static right-0 top-[33px] s:top-[50px]">
+        <input type="checkbox" id="check" className="hidden" />
+        <label
+          htmlFor="check"
+          className="navdrop-ctn block s:hidden absolute right-5"
+          onClick={() => setNavBarToggle(!navBarToggle)}
+        >
+          {navBarToggle ? (
+            <FaBars size={25} className="navdrop-1" />
+          ) : (
+            <RiCloseFill size={25} className="navdrop-2" />
+          )}
+        </label>
+
+        {/* <ul className="s:flex w-[200px] s:w-[initial] mt-[51px] s:mt-[10px]  p-4 s:p-[initial] justify-between menu-main bg-[#edf5fa] rounded s:bg-[initial] s:h-[initial] h-[242px]">
+          {data.map((item, key) => (
+            <li key={key} className="my-3 s:my-[initial] text-sm font-semibold">
+              {item.name}
+            </li>
+          ))}
+        </ul> */}
       </div>
     </nav>
   );
