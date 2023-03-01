3d66ff36fe369f5e16040f6cefb1fd20cc540e49
Chibuike Ewenike <sopewenike@yahoo.com>
2023-03-01 19:02:48 +0100

    features styled

--- src/components/Featuredarticles.js
+++ src/components/Featuredarticles.js
@@ -11,21 +11,33 @@ function Featuredarticles() {
   ];
 
   return (
-    <section className="w-full">
-      <div className="px-4 mx-auto sm:max-w-[600px] md:max-w-[760px] lg:max-w-[1010px] xl:max-w-[1280px] py-12 lg:flex">
-        <div className="flex gap-5 w-[90%] mx-auto">
-          {features.map((item) => (
-            <div className="basis-[30%]">
-              <div
-                style={{
-                  backgroundImage:
-                    "linear-gradient(rgba(0,0,0,0.827),rgba(0,0,0,0.3)),url('https://www.penfed.org/content/dam/penfed/blogimages/2021/lc-proven-budgeting-strategies.jpg')",
-                }}
-                className="text-[white] text-[1.5rem] bg-right bg-cover  h-[220px] w-full "
-              ></div>
-              <div className="text-center mt-5">{item.title}</div>
-            </div>
-          ))}
+    <section className="w-full bg-[#f4f4f4]">
+      <div className="py-[48px]">
+        <div className="px-4 mx-auto">
+          <h1 className="font-semibold text-center mb-[40px] text-[2rem]">
+            Featured Articles
+          </h1>
+          <div className="md:flex gap-5 max-w-[315px] xs:max-w-[544px] md:max-w-[720px] penfedL:max-w-[960px] lg:max-w-[1152px] mx-auto flex-wrap justify-center">
+            {features.map((item) => (
+              <div className="basis-[45%] lg:basis-[30%]">
+                <div
+                  style={{
+                    backgroundImage:
+                      "url('https://www.penfed.org/content/dam/penfed/blogimages/2021/lc-proven-budgeting-strategies.jpg')",
+                  }}
+                  className="text-[white] text-[1.5rem] bg-center bg-cover  h-[220px] w-full "
+                ></div>
+                <div className="text-center p-4">
+                  <p className="text-center text-[1.13rem] mt-5 font-semibold">
+                    {item.title}
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
+          <button className="bg-[#047dba] flex justify-center  mx-auto cursor-pointer py-2 px-[1.375rem] text-base  text-[#ffffff] font-[700] border rounded-[10px] border-solid border-[#ffffff]">
+            See All Articles
+          </button>
         </div>
       </div>
     </section>
