f40f0c84156d00efdef9afd39505dbf55075d698
Chibuike Ewenike <sopewenike@yahoo.com>
2023-03-02 11:29:17 +0100

    login finished

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -11,6 +11,7 @@ export default function Login() {
   const [formValue, setFormData] = useState(InitiaState);
   const handleSubmit = (event) => {
     event.preventDefault(); // prevent the default form submission behavior
+    console.log("Details of user", formValue);
     navigate("/");
   };
 
@@ -18,16 +19,21 @@ export default function Login() {
     const { name, value } = e.target;
     setFormData((prev) => ({ ...prev, [name]: value }));
   }
+  const data = [
+    { name: "NCUA_logo.svg", url: "" },
+    { name: "Norton_logo.png", url: "" },
+    { name: "EHL_logo_white.svg", url: "" },
+  ];
   return (
     <div className="relative flex flex-col justify-center min-h-screen overflow-hidden">
-      <div className="w-full p-6 m-auto bg-white rounded-md shadow-md lg:max-w-xl">
-        <div className="flex justify-center">
+      <div className="w-full p-6 m-auto bg-white rounded-md  md:w-[50%] mx-auto">
+        <div className="flex justify-center mt-[96px] mb-20">
           <img
             src={process.env.PUBLIC_URL + "/logo.png"}
             className="w-[238px] h-[61.22px]"
           />
         </div>
-        <form className="mt-6">
+        <form className="mt-6 ">
           <div className="mb-2">
             <label
               for="username"
@@ -73,6 +79,35 @@ export default function Login() {
           <Link to="/" className="cursor-pointer">
             setup your online account or get help signing in.
           </Link>
+        </p>
+      </div>
+      <div className="bg-[#59606e] px-10 pt-10 text-[#ffffff]">
+        <div className="text-center text-[10px]">
+          <p className=" ">
+            This credit union is federally insured by the National Credit Union
+            Administration. Rates are current as of March 2023 unless otherwise
+            noted and are subject to change.
+          </p>
+          <h5 className="mt-5 text-[#d0d0d0] underline">Privacy Policy</h5>
+        </div>
+        <div className="[&>*:nth-child(3)]:order-last flex justify-center items-center mx-auto py-[40px]">
+          {data.map((item, key) => (
+            <div key={key} className="basis-[20%] mx-auto">
+              <img
+                src={process.env.PUBLIC_URL + `/${item.name}`}
+                className="h-[50px] w-[90px] mx-auto"
+                alt="logo"
+              />
+            </div>
+          ))}
+          <h2 className="basis-[20%] mx-auto text-[10px] text-[white]">
+            ABOUT SSL <span className="block">CERTIFICATES</span>
+          </h2>
+        </div>
+      </div>
+      <div className="bg-[#384149] h-[40px] flex justify-center items-center ">
+        <p className="text-[#d0d0d0] text-[0.8125rem]">
+          ©2023 Pentagon Federal Credit Union
         </p>
       </div>
     </div>
